0fb5327e629631336fdef69ecfa8bee5b9815c4c
jaeyoungchoi1 <choijy5847@naver.com>
2025-01-12 13:06:26 +0900

    chore: favicon & pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: { transformMixedEsModules: true } // Change
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'logo_white.png'],
+      manifest: {
+        name: 'Sperm Nest',
+        short_name: 'SpermNest',
+        description: '블록체인 기반 정자 기증 네트워크',
+        theme_color: '#2563eb',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
